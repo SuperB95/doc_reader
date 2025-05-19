fbb138571fc706d775be0831c8e66ec928fcb418
Kontoli <babeni119@gmail.com>
2025-05-19 16:12:11 +0200

    Update UploadActivity.java

--- app/src/main/java/com/example/doc_reader/UploadActivity.java
+++ app/src/main/java/com/example/doc_reader/UploadActivity.java
@@ -59,6 +59,7 @@ public class UploadActivity extends AppCompatActivity {
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == PERMISSION_REQUEST_CODE) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 openFilePicker();
